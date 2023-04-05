136cef7e6c6dce189b1cc87efefe3bc90cac6ec0
Florian <florian.von.bracht@apteco.de>
2023-04-05 13:03:58 +0200

    Initial Commit for ConvertStrings

---
+++ ConvertStrings/ConvertStrings/Public/Get-StringHash.ps1
@@ -0,0 +1,82 @@
+<#
+
+This is a way of hashing strings
+Please be aware, that algorithms like HMAC need a key that
+The hashing algorithm should be in this class System.Security.Cryptography
+
+# examples
+
+# This results in 872e4e50ce9990d8b041330c47c9ddd11bec6b503ae9386a99da8584e9bb12c4
+Get-StringHash -inputString "HelloWorld" -hashName "SHA256"
+
+# This results in b9e217df88dc1bc96c1e69e1b09a798d6efe0ef69cd3511e7f4becd319fe6036
+Get-StringHash -inputString "HelloWorld" -hashName "HMACSHA256" -key "GoGoGo"
+
+
+#>
+Function Get-StringHash {
+
+    [cmdletbinding()]
+    param(
+         [Parameter(Mandatory=$true)][String]$InputString
+        ,[Parameter(Mandatory=$true)][String]$HashName
+        ,[Parameter(Mandatory=$false)][String]$Salt = ""
+        ,[Parameter(Mandatory=$false)][String]$Key = ""
+        ,[Parameter(Mandatory=$false)][switch]$Uppercase = $false
+        ,[Parameter(Mandatory=$false)][switch]$KeyIsHex = $false
+        ,[Parameter(Mandatory=$false)][switch]$ReturnBytes = $false
+    )
+
+    Process {
+
+        #-----------------------------------------------
+        # GENERATE BYTES HASH
+        #-----------------------------------------------
+
+        # Add salt if needed
+        $string = $InputString + $Salt
+
+        # Choose algorithm
+        $alg = [System.Security.Cryptography.HashAlgorithm]::Create($HashName)
+
+        # Change key, e.g. for HMACSHA256
+        if ( $Key -ne "" ) {
+            if ( $KeyIsHex ) {
+                $alg.key = Convert-HexToByteArray -HexString $key
+            } else {
+                $alg.key = [Text.Encoding]::UTF8.GetBytes($key)
+            }
+        }
+
+        $bytes = $alg.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($string))
+
+        
+        #-----------------------------------------------
+        # RETURN
+        #-----------------------------------------------
+
+        if ( $ReturnBytes -eq $true ) {
+
+            $bytes
+
+        } else {
+
+            # Create bytes from string and hash
+            $res = Convert-ByteArrayToHex -Bytes $bytes
+
+            # Transform uppercase, if needed, and return the result
+            if ( $Uppercase ) {
+                $res.ToUpper()
+            } else {
+                $res
+            }
+
+        }
+
+
+
+    }
+
+}
+
+
---
+++ ConvertStrings/ConvertStrings/Public/Replace-Tokens.ps1
@@ -0,0 +1,27 @@
+﻿
+<#
+New string function to replace multiple strings in a string. Multiple replacements can be defined as a hashtable with the string to replace a key and value as the replacement
+#>
+
+Function Set-Tokens {
+    
+    param(
+         [Parameter(Mandatory=$true)][array]$InputString
+        ,[Parameter(Mandatory=$true)][Hashtable]$Replacements
+    )
+
+    Process {
+        
+        $newString = $InputString
+
+        $Replacements.Keys | ForEach {
+            $key = $_
+            $newString = $newString -replace $key, $Replacements.$key
+        }
+
+        # Return
+        return $newString
+        
+    }
+
+}
