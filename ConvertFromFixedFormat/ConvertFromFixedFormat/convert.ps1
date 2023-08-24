fd28ed4f8854f56b8eae208eb346a8afa1a7746f
Florian <florian.von.bracht@apteco.de>
2023-08-24 10:01:31 +0200

    Remove some code to bring this into other branches

--- ConvertFromFixedFormat/ConvertFromFixedFormat/convert.ps1
+++
@@ -1,7 +0,0 @@
-$f = "C:\Users\Florian\Downloads\BLZ.txt"
-
-#get-content -path $f -encoding default
-
-# https://stackoverflow.com/questions/39897926/convert-fixed-width-txt-file-to-csv-set-content-or-out-file-append
-
-$c.substring(0,10).trim()
