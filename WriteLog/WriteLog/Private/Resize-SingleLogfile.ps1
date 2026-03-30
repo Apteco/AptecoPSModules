8ec6422551ef6be94c90279a43a4fd0bd4dadfb5
Florian <florian.von.bracht@apteco.de>
2026-03-30 12:05:52 +0200

    0.10.3 fix for WriteLog module
    Fixed a scalar output problem for PowerShell 5.1 when calling Get-AdditionalLog with one entry

--- WriteLog/WriteLog/Private/Resize-SingleLogfile.ps1
+++ WriteLog/WriteLog/Private/Resize-SingleLogfile.ps1
@@ -2,31 +2,44 @@ Function Resize-SingleLogfile {
 
     [cmdletbinding()]
     param(
-       [Parameter(Mandatory=$true)][String]$Path
-      ,[Parameter(Mandatory=$true)][int]$RowsToKeep
+
+       [Parameter(Mandatory=$true)]
+       [String]$Path
+      
+       ,[Parameter(Mandatory=$true)]
+       [int]$RowsToKeep
+
     )
 
-    If ( ( Test-Path -Path $Path -IsValid ) -eq $false ) {
-        Write-Error -Message "The path '$( $Path )' is invalid."
-        return
+    Begin {
+        
+        If ( ( Test-Path -Path $Path -IsValid ) -eq $false ) {
+            Write-Error -Message "The path '$( $Path )' is invalid."
+            return
+        }
+
+        If ( ( Test-Path -Path $Path ) -eq $false ) {
+            Write-Verbose -Message "Skipping '$( $Path )' - file does not exist yet."
+            return
+        }
+
     }
 
-    If ( ( Test-Path -Path $Path ) -eq $false ) {
-        Write-Verbose -Message "Skipping '$( $Path )' — file does not exist yet."
-        return
+    Process {
+
+        # Create a temporary file
+        $tmpdir = Get-TemporaryPath
+        $tempFile = Join-Path -Path $tmpdir -ChildPath "$( [guid]::newguid().toString() ).tmp"
+
+        # Write only last lines to the new file
+        Get-Content -Tail $RowsToKeep -Encoding utf8 -Path $Path | Set-Content -Path $tempFile -Encoding utf8
+
+        # Delete original file and replace with trimmed version
+        Remove-Item -Path $Path
+        Move-Item -Path $tempFile -Destination $Path
+
+        Write-Verbose -Message "Resized '$( $Path )' to last $( $RowsToKeep ) lines"
+
     }
-
-    # Create a temporary file
-    $tmpdir = Get-TemporaryPath
-    $tempFile = Join-Path -Path $tmpdir -ChildPath "$( [guid]::newguid().toString() ).tmp"
-
-    # Write only last lines to the new file
-    Get-Content -Tail $RowsToKeep -Encoding utf8 -Path $Path | Set-Content -Path $tempFile -Encoding utf8
-
-    # Delete original file and replace with trimmed version
-    Remove-Item -Path $Path
-    Move-Item -Path $tempFile -Destination $Path
-
-    Write-Verbose -Message "Resized '$( $Path )' to last $( $RowsToKeep ) lines."
 
 }
