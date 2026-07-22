84e38ee560df75df4244cab42ee1e4a7e2409164
Florian <florian.von.bracht@apteco.de>
2026-07-22 13:21:35 +0200

    Adding and improving pester tests for MergeHashtable and MergePSCustomObject

--- MergePSCustomObject/Test/Merge-PSCustomObject.Tests.ps1
+++ MergePSCustomObject/Test/Merge-PSCustomObject.Tests.ps1
@@ -1,6 +1,12 @@
-﻿BeforeAll {
+BeforeAll {
     # Import the module
-    Import-Module $PSScriptRoot/../"MergePSCustomObject"
+    Import-Module $PSScriptRoot/../"MergePSCustomObject" -Force
+
+    # Sibling module needed for the -MergeHashtables recursion tests further down
+    if ( -not ( Get-Module -ListAvailable -Name "MergeHashtable" ) ) {
+        Install-Module -Name MergeHashtable -Force -Scope CurrentUser -Repository PSGallery
+    }
+    Import-Module MergeHashtable -Force
 
     # From https://stackoverflow.com/questions/50870891/how-to-compare-two-arrays-with-custom-objects-in-pester
     # Changed to send back the difference
@@ -10,11 +16,7 @@
         )
         $Property = ($a | Select-Object -First 1).PSObject.Properties | Select-Object -Expand Name
         $Difference = Compare-Object $b $a -Property $Property
-        #Try {
-            "$($Difference | Select-Object -First 1)" #| Should -BeNullOrEmpty
-        #} Catch {
-        #    $PSCmdlet.WriteError($_)
-        #}
+        "$($Difference | Select-Object -First 1)" #| Should -BeNullOrEmpty
     }
 
 }
@@ -25,7 +27,6 @@ Describe 'Merge-PSCustomObject' {
 
         It 'Plain objects without nesting and other datatypes and flags' {
 
-            # Define the test objects
             $left = [PSCustomObject]@{
                 "firstname" = "Florian"
                 "lastname" = "Friedrichs"
@@ -43,15 +44,23 @@ Describe 'Merge-PSCustomObject' {
 
             $result = Merge-PSCustomObject -Left $left -right $right
 
-            #$result | Should -Be $expectedResult # Does not work with objects
-
-            # Delivers something back if the property values are differing
             $result | Should-BeObject $expectedResult | Should -BeNullOrEmpty
 
-            # Compare with json? better for nested objects?
-            #(ConvertTo-Json $result) | Should -Be (ConvertTo-Json $expectedResult)
+        }
+
+        It 'Does not add properties from right that are not present on left' {
+
+            $left = [PSCustomObject]@{ "firstname" = "Florian" }
+            $right = [PSCustomObject]@{ "Street" = "Schaumainkai 87" }
+
+            $result = Merge-PSCustomObject -Left $left -right $right
+
+            $result.PSObject.Properties.Name | Should -Not -Contain "Street"
+            $result.firstname | Should -Be "Florian"
 
         }
+
+    }
 
     Context 'Extra flags' {
 
@@ -75,25 +84,159 @@ Describe 'Merge-PSCustomObject' {
 
             $result = Merge-PSCustomObject -Left $left -right $right -AddPropertiesFromRight
 
-            # Delivers something back if the property values are differing
             $result | Should-BeObject $expectedResult | Should -BeNullOrEmpty
 
         }
 
     }
 
+    Context 'Nested PSCustomObject values' {
+
+        It 'Overwrites the nested object with the one from right by default' {
+
+            $left = [PSCustomObject]@{
+                "address" = [PSCustomObject]@{ "street" = "Schaumainkai 87" }
+            }
+            $right = [PSCustomObject]@{
+                "address" = [PSCustomObject]@{ "postcode" = 60596 }
+            }
+
+            $result = Merge-PSCustomObject -Left $left -right $right
+
+            $result.address.PSObject.Properties.Name | Should -Not -Contain "street"
+            $result.address.postcode | Should -Be 60596
+
+        }
+
+        It 'Merges the nested object recursively with -MergePSCustomObjects' {
+
+            $left = [PSCustomObject]@{
+                "address" = [PSCustomObject]@{ "street" = "Schaumainkai 87" }
+            }
+            $right = [PSCustomObject]@{
+                "address" = [PSCustomObject]@{ "postcode" = 60596 }
+            }
+
+            $result = Merge-PSCustomObject -Left $left -right $right -MergePSCustomObjects -AddPropertiesFromRight
+
+            $result.address.street | Should -Be "Schaumainkai 87"
+            $result.address.postcode | Should -Be 60596
+
+        }
+
+        It 'Keeps the filled left object when the nested right object is empty (regression)' {
+
+            $left = [PSCustomObject]@{
+                "address" = [PSCustomObject]@{ "firstname" = "Flo" }
+            }
+            $right = [PSCustomObject]@{
+                "address" = [PSCustomObject]@{}
+            }
+
+            $result = Merge-PSCustomObject -Left $left -right $right -MergePSCustomObjects
+
+            $result.address.firstname | Should -Be "Flo"
+
+        }
+
     }
 
+    Context 'Nested Hashtable values (combination with Merge-Hashtable)' {
 
+        It 'Overwrites the nested hashtable with the one from right by default' {
 
+            $left = [PSCustomObject]@{
+                "address" = [hashtable]@{ "street" = "Schaumainkai 87" }
+            }
+            $right = [PSCustomObject]@{
+                "address" = [hashtable]@{ "postcode" = 60596 }
+            }
 
+            $result = Merge-PSCustomObject -Left $left -right $right
+
+            $result.address.Keys | Should -Not -Contain "street"
+            $result.address["postcode"] | Should -Be 60596
+
+        }
+
+        It 'Merges the nested hashtable recursively via Merge-Hashtable with -MergeHashtables' {
+
+            $left = [PSCustomObject]@{
+                "address" = [hashtable]@{ "street" = "Schaumainkai 87" }
+            }
+            $right = [PSCustomObject]@{
+                "address" = [hashtable]@{ "postcode" = 60596 }
+            }
+
+            $result = Merge-PSCustomObject -Left $left -right $right -MergeHashtables -AddPropertiesFromRight
+
+            $result.address | Should -BeOfType [hashtable]
+            $result.address["street"] | Should -Be "Schaumainkai 87"
+            $result.address["postcode"] | Should -Be 60596
+
+        }
+
+    }
+
+    Context 'Arrays and ArrayLists with -MergeArrays' {
+
+        It 'Merges [Array] properties and removes duplicates' {
+
+            $left = [PSCustomObject]@{ "tags" = [Array]@("a","b") }
+            $right = [PSCustomObject]@{ "tags" = [Array]@("b","c") }
+
+            $result = Merge-PSCustomObject -Left $left -right $right -MergeArrays
+
+            @( $result.tags | Sort-Object ) | Should -Be @("a","b","c")
+
+        }
+
+        It 'Merges [ArrayList] properties and removes duplicates' {
+
+            $left = [PSCustomObject]@{ "tags" = [System.Collections.ArrayList]@("a","b") }
+            $right = [PSCustomObject]@{ "tags" = [System.Collections.ArrayList]@("b","c") }
+
+            $result = Merge-PSCustomObject -Left $left -right $right -MergeArrays
+
+            $result.PSObject.Properties.Name | Should -Contain "tags"
+            @( $result.tags | Sort-Object ) | Should -Be @("a","b","c")
+
+        }
+
+    }
+
+    Context 'Deep combination of PSCustomObject and Hashtable nesting' {
+
+        It 'Merges a PSCustomObject that contains a Hashtable that contains a PSCustomObject' {
+
+            $left = [PSCustomObject]@{
+                "firstname" = "Florian"
+                "settings" = [hashtable]@{
+                    "theme" = "dark"
+                    "profile" = [PSCustomObject]@{ "name" = "Orbit" }
+                }
+            }
+
+            $right = [PSCustomObject]@{
+                "lastname" = "von Bracht"
+                "settings" = [hashtable]@{
+                    "language" = "de"
+                    "profile" = [PSCustomObject]@{ "owner" = "Apteco" }
+                }
+            }
+
+            $result = Merge-PSCustomObject -Left $left -Right $right -AddPropertiesFromRight -MergePSCustomObjects -MergeHashtables
+
+            $result.firstname | Should -Be "Florian"
+            $result.lastname | Should -Be "von Bracht"
+            $result.settings | Should -BeOfType [hashtable]
+            $result.settings["theme"] | Should -Be "dark"
+            $result.settings["language"] | Should -Be "de"
+            $result.settings["profile"].name | Should -Be "Orbit"
+            $result.settings["profile"].owner | Should -Be "Apteco"
+
+        }
+
+    }
 
 }
-
-
-
-
-
-
-
-
